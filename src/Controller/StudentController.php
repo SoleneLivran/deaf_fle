f79fe6b20238fc259a8b73deb779351a55f1d839
Solène Livran <s.livran@gmail.com>
2021-02-15 20:26:46 +0100

    create student update endpoint

--- src/Controller/StudentController.php
+++ src/Controller/StudentController.php
@@ -6,8 +6,11 @@ use App\Entity\Group;
 use App\Entity\Student;
 use App\Repository\StudentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * @Route("/api")
@@ -47,6 +50,30 @@ class StudentController extends AbstractController
             Response::HTTP_OK,
             [],
             ["groups" => ["student:view"]]
+        );
+    }
+
+    /**
+     * @Route("/student/{id}", name="student_update", methods={"PATCH"}, requirements={"id"="\d+"})
+     * @param Request $request
+     * @param Student $student
+     * @param SerializerInterface $serializer
+     * @return Response
+     */
+    public function update(Request $request, Student $student, SerializerInterface $serializer) : Response
+    {
+        $submittedData = $request->getContent();
+
+        $serializer->deserialize($submittedData, Student::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $student]);
+
+        $this->getDoctrine()->getManager()->flush();
+
+        return $this->json(
+            [
+                "success" => true,
+                "text" => $student->getText(),
+            ],
+            Response::HTTP_OK
         );
     }
 }
